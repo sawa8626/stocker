1c727b97bfa65d6eb2b1ca108fa51d85878bd38d
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-10-20 15:25:52 +0900

    Create items.create view

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,3 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    アイテム登録フォーム
+                </div>
+                <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <form method="POST" action="">
+                @csrf
+                    <div class="form-group row">
+                        <label for="inputName" class="col-sm-3 col-form-label">アイテム名</label>
+                        <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inputName" placeholder="アイテム名">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputGenre" class="col-sm-3 col-form-label">ジャンル</label>
+                        <div class="col-sm-9">
+                        <input type="text" class="form-control" id="inputGenre" placeholder="ジャンル">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputPrice" class="col-sm-3 col-form-label">価格(¥)</label>
+                        <div class="col-sm-9">
+                        <input type="number" class="form-control" id="inputPrice" placeholder="価格">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputDate" class="col-sm-3 col-form-label">使用開始日</label>
+                        <div class="col-sm-9">
+                        <input type="date" class="form-control" id="inputDate">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">登録する</button>
+                        </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
